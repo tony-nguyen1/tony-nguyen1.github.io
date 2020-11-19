333c248c8799ea2579d1d07f25bc98086585bfd7
tony-nguyen1 <tony.nguyen@etu.umontpellier.fr>
2020-11-19 14:33:04 +0100

    carousel dans methode de paiement ajoute

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,5 +1,3 @@
-console.log("Hi");
-
 document.addEventListener('DOMContentLoaded', function() {
 
   M.AutoInit();
@@ -10,4 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var elems = document.querySelectorAll('.tooltipped');
   var instances = M.Tooltip.init(elems, options);
+
+  var options2 = {
+   shift: 100,
+   dist: -5
+ };
+  var elems2 = document.querySelectorAll('.carousel');
+  var instances2 = M.Carousel.init(elems2, options2);
 });
