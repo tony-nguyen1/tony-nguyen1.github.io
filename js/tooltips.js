1d35ebc6fde2bb7742b4759d4b1baad7f8cd87a0
tony-nguyen1 <tony.nguyen@etu.umontpellier.fr>
2020-11-21 15:02:46 +0100

    carousel + redirection .htaccess http to https

--- js/tooltips.js
+++ js/tooltips.js
@@ -10,8 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Tooltip.init(elems, options);
 
   var options2 = {
-   shift: 100,
-   dist: -5
+   shift: 0,
+   dist: 0,
+   padding: 165,
+   numVisible: 5,
+   fullWidth: true,
+   indicators: true
  };
   var elems2 = document.querySelectorAll('.carousel');
   var instances2 = M.Carousel.init(elems2, options2);
